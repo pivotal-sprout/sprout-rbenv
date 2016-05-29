4b380b08de32ff85951d6057ba5b4dc93644272f
Dan Wendorf <dwendorf@pivotal.io>
2016-05-29 11:46:49 -0700

    Update to latest Ruby

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ node.default['sprout']['rbenv'] = {
   'home' => "#{node['sprout']['home']}/.rbenv",
   'command' => '/usr/local/bin/rbenv',
   'rubies' => {
-      '2.2.3' => {},
+      '2.3.1' => {},
     },
-  'default_ruby' => '2.2.3',
+  'default_ruby' => '2.3.1',
 }
