0ff18a6ed878be2c2326ffc50810315f85d1e386
Dan Wendorf <dwendorf@pivotal.io>
2016-05-31 12:49:27 -0700

    Address RuboCop errors

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ node.default['sprout']['rbenv'] = {
   'home' => "#{node['sprout']['home']}/.rbenv",
   'command' => '/usr/local/bin/rbenv',
   'rubies' => {
-      '2.3.1' => {},
-    },
-  'default_ruby' => '2.3.1',
+    '2.3.1' => {}
+  },
+  'default_ruby' => '2.3.1'
 }
